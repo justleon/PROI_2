3545ac25d0dba622ed66692a57a2709017a55206
Łukasz Pokorzyński <maniak656@gmail.com>
2019-03-09 16:58:21 +0100

    Slight changes in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,5 +10,5 @@ int main()
     std::cout << "num1 + num2: " << num1 << "num2 - num1: " << num2 << std::endl;
     num1 += num2;
     num2 -= num1;
-    std::cout << "num1 += num2:" << num1 << "num2 -= num1" << num2 << std::endl;
+    std::cout << "num1 += num2: " << num1 << "num2 -= num1: " << num2 << std::endl;
 }
